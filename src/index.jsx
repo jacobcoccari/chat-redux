b0479bf3c8f1b91da724d378452caa870bf54acf
jacob coccari <jacob.coccari@gmail.com>
2019-06-03 19:22:02 -0700

    added the middlewares redux-promise and redux-logger

--- src/index.jsx
+++ src/index.jsx
@@ -2,8 +2,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, combineReducers } from 'redux';
-
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import {logger} from 'redux-logger';
+import reduxPromise from 'redux-promise';
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
@@ -21,9 +22,11 @@ const reducers = combineReducers({
   changeMe: (state = null, action) => state
 });
 
+const middlewares = applyMiddleware(reduxPromise, logger);
+
 // render an instance of the component in the DOM
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={createStore(reducers, {}, middlewares)}>
     <App />
   </Provider>,
   document.getElementById('root')
