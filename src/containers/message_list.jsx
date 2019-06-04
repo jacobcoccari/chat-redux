be9037f96cf0eb10809e1dd3ce0efa46af04ef71
jacob coccari <jacob.coccari@gmail.com>
2019-06-03 19:31:27 -0700

    added the meessage_list container

---
+++ src/containers/message_list.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+class MessageList extends component {
+  renderlist = () => {
+    return null;
+  }
+
+  render() {
+    return null;
+    //call the renderlist function
+  }
+
+  function mapReduxStateToProps(state) {
+    return {
+      null;
+    }
+  }
+}
