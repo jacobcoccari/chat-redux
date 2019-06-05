42410a483e9ff3713724f9c1e5c00ebd805ff433
jacob coccari <jacob.coccari@gmail.com>
2019-06-05 10:14:47 -0700

    the channels now render through mapping the state channels onto a renderchannel function

--- src/containers/channel_list.jsx
+++ src/containers/channel_list.jsx
@@ -4,14 +4,27 @@ import { connect } from 'react-redux';
 
 
 class ChannelList extends Component {
+  renderChannel = (channel) => {
+    return (
+      <li key={channel}
+      classname={channel === this.props.selectedChannel ? 'active' : null}
+      onClick={() => this.handleClick(channel)}>
+        {channel}
+      </li>
+      );
+  }
+
   render() {
-    return <h1>{this.props.channels}</h1>
+    return <div className="channels-container">
+      <ul>{this.props.channels.map(this.renderChannel)}</ul>
+     </div>
   }
 }
 
 function mapReduxStateToProps(state) {
   return {
-    channels: state.channels
+    channels: state.channels,
+    selectedChannel: state.selectedChannel
   }
 }
 
